9da19d11a55aca53f45f1ca86c8b0a7101fdf0a3
Liam Bailey <ljbailey17@gmail.com>
2023-07-23 18:45:59 +0100

    Add book, remove book, list book, checked_out book

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,22 +1,21 @@
 from flask import Blueprint, render_template, request, redirect
-from models.book_list import book_list, add_new_book, delete_book 
+
 from models.book import *
+from models.book_list import books, add_new_book, delete_book
 
-book_blueprint = Blueprint("book", __name__)
+book_blueprint = Blueprint("books", __name__)
 
-
-# INDEX is show all 
-
-@book_blueprint.route('/books')
+@book_blueprint.route("/books")
 def index():
-    return render_template("index.jinja", title = "LIBRARY", book_list=book_list)
+    return render_template("index.jinja", title="LIBRARY", book_list=books)
 
-@book_blueprint.route('/books', methods = ["POST"])
+@book_blueprint.route("/books", methods=['POST'])
 def add_book():
     book_title = request.form['title']
     book_author = request.form['author']
     book_genre = request.form['genre']
-    new_book = Book(book_title, book_author, book_genre)
+    checked_out = False 
+    new_book = Book(book_title, book_author, book_genre, checked_out)
     add_new_book(new_book)
     return redirect('/books')
 
@@ -26,23 +25,16 @@ def delete(title):
     return redirect('/books')
 
 
-# @book_blueprint.route("/books", methods=['POST'])
-# def add_book():
-#     book_title = request.form['title']
-#     book_author = request.form['author']
-#     book_genre = request.form['genre']
-#     new_book = Book(book_title, book_author, book_genre)
-#     add_new_book(new_book)
-#     return redirect('/books')
-
-# @book_blueprint.route('/books/delete/<title>', methods=['POST'])
-# def delete(title):
-#     delete_book(title)
-#     return redirect('/books')
-
 # Provide the following functionality:
 
-# List all Books
-# Show an individual Book
-# Add a new Book to the Library.
-# Remove a Book from the Library
+# List all Books - DONE 
+# Show an individual Book - cannot figure out - indexes??? 
+# Add a new Book to the Library. - DONE
+# Remove a Book from the Library - DONE (but only on bottom part - how to copy same as above??? 
+
+# Extension - show if available or checked out 
+# IF BOOK IS AVAILABLE, SHOW AS AVAILBABLE IF NOT SHOW AS UNAVAILABLE
+# if checked_out == true then return/print appropiate outcome 
+
+#  add property, checked_out as True or False, a function to print whether true or false 
+
--- models/book_list.py
+++ models/book_list.py
@@ -1,22 +1,22 @@
-from models.book import Book 
+from models.book import *
 
-book1 = Book("Game of Thrones", "George RR Martin", "Fantasy")
-book2 = Book("Japan", "Lonely Planet", "Travel Guide")
-book3 = Book("Klara and the Sun", "Kazuo Ishiguro", "Science Fiction")
+book1 = Book("Game of Thrones", "George RR Martin", "Fantasy", True)
+book2 = Book("Japan", "Lonely Planet", "Travel Guide", True )
+book3 = Book("Klara and the Sun", "Kazuo Ishiguro", "Science Fiction", False)
 
-book_list = [book1, book2, book3]
+books = [book1, book2, book3]
 
 def add_new_book(book):
-    book_list.append(book)
+    books.append(book)
 
 def delete_book(book_title):
     book_to_delete = None
-    for book in book_list:
+    for book in books:
         if book.title == book_title:
             book_to_delete = book
             break
 
-    book_list.remove(book_to_delete)
+    books.remove(book_to_delete)
 
 
 
